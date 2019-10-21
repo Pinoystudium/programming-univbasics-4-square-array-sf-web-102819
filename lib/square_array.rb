e9b4f7c304f23205035fc4410bec5e00989ce01c
Michael Espina <michael_espina@yahoo.com>
2019-10-21 07:00:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,13 @@ def square_array(array)
  
  while counter < array.length do
    square = (array[counter] * array[counter])
+<<<<<<< HEAD
    array.push(square)
    puts array
+=======
+   puts square
+   return square
+>>>>>>> 186c75c3d0715ee112d1b7a68bdc993bcccdbd58
    counter += 1
   end
 end
